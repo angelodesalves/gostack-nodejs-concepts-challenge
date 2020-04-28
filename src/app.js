83571177f4aba4c2eb4142c3f112cab2d0c5d92d
Angelo de Salves <angelopeto@yahoo.com.br>
2020-04-28 00:32:30 -0300

    Setted initial registers

--- src/app.js
+++ src/app.js
@@ -8,7 +8,48 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = [
+	{
+		"id": "9c4f9cda-39df-4fc9-b743-b75d9bc28394",
+		"title": "Desafio conceitos react",
+		"url": "https://github.com/angelodesalves/gostack-react-concepts-challenge",
+		"techs": [
+			"react"
+		],
+		"likes": 9
+	},
+	{
+		"id": "081deb83-2dee-4595-920f-aa8132fa9b60",
+		"title": "Desafio conceitos Node",
+		"url": "https://github.com/angelodesalves/gostack-react-concepts-challenge",
+		"techs": [
+		"Node",
+			"Express"
+		],
+		"likes": 13
+	},
+	{
+		"id": "159eb0dd-cd69-49ee-8395-5170d2e51f11",
+		"title": "Desafio conceitos Node Native",
+		"url": "https://github.com/angelodesalves/gostack-react-concepts-challenge",
+		"techs": [
+			"React",
+			"React Native",
+			"Android",
+			"IOS"
+		],
+		"likes": 7
+	},
+	{
+		"id": "0efd10fe-4247-4bf9-8865-f5183745e65f",
+		"title": "Conceitos Typescript",
+		"url": "https://github.com/angelodesalves/gostack-react-concepts-challenge",
+		"techs": [
+			"Typescript"
+		],
+		"likes": 21
+	}
+];
 
 const valideIdRepository = (request, response, next) => {
 	const { id } = request.params;
